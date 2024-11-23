0af947e347e98018049ed5840d08fb7581cd6cd9
kittanutp <kittanut.pong@gmail.com>
2024-11-23 23:58:00 +0700

    phase 2 ver 1

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 type Question struct {
@@ -15,30 +17,45 @@ type Question struct {
 type Quiz struct {
 	Questions chan Question
 	Counter   int
+	Done      chan bool
+	Timer     *time.Timer
 }
 
-const path = "problems.csv"
-
 func main() {
-	quiz := NewQuiz()
-	go quiz.createQuizFromCSV()
-	fmt.Println("Start Quiz")
-	var i string
-	var resp string
-	for q := range quiz.Questions {
-		fmt.Printf("Q >> %v\n", q.Question)
-		fmt.Print("Your Answer: ")
-		fmt.Scanln(&i)
-		if i == q.Answer {
-			resp = "Correct"
-			quiz.Counter += 1
 
-		} else {
-			resp = "Wrong"
+	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz in seconds")
+	flag.Parse()
+
+	quiz := NewQuiz()
+
+	go quiz.createQuizFromCSV(*csvFilename)
+
+	var i string
+	fmt.Println("Press Any Key to start quiz")
+	fmt.Scanln(&i)
+
+	fmt.Printf("Quiz will take %v seconds\n", *timeLimit)
+	quiz.Timer = time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+problemloop:
+	for {
+		select {
+		case <-quiz.Timer.C:
+			fmt.Println("\nTime's up!")
+			fmt.Println("Total Score:", quiz.Counter)
+			break problemloop
+		case q, ok := <-quiz.Questions:
+			if !ok {
+				fmt.Println("Quiz completed!")
+				fmt.Println("Total Score:", quiz.Counter)
+				return
+			}
+			if quiz.askQuestion(q) {
+				quiz.Counter++
+			}
 		}
-		fmt.Println("Your Answer is", resp)
 	}
-	fmt.Println("Total Score", quiz.Counter)
 }
 
 func NewQuiz() *Quiz {
@@ -47,12 +64,13 @@ func NewQuiz() *Quiz {
 	}
 }
 
-func (quiz *Quiz) createQuizFromCSV() {
-	file, err := os.Open(path)
-
+func (quiz *Quiz) createQuizFromCSV(csvFilename string) {
+	file, err := os.Open(csvFilename)
 	if err != nil {
 		log.Fatal("Error while reading the file", err)
 	}
+	defer file.Close()
+
 	reader := csv.NewReader(file)
 	for {
 		record, err := reader.Read()
@@ -70,6 +88,31 @@ func (quiz *Quiz) createQuizFromCSV() {
 			Answer:   record[1],
 		}
 	}
-	defer file.Close()
-	defer close(quiz.Questions)
+	close(quiz.Questions)
+}
+
+func (quiz *Quiz) askQuestion(q Question) bool {
+	fmt.Printf("Q >> %v\n", q.Question)
+	fmt.Print("Your Answer: ")
+
+	answerReceived := make(chan string, 1)
+	go func() {
+		var input string
+		fmt.Scanln(&input)
+		answerReceived <- input
+	}()
+
+	select {
+	case <-quiz.Timer.C:
+		quiz.Done <- true
+		fmt.Println("Interupt!")
+		return false
+	case input := <-answerReceived:
+		if input == q.Answer {
+			fmt.Println("Your Answer is correct")
+			return true
+		}
+		fmt.Println("Your Answer is wrong")
+		return false
+	}
 }
